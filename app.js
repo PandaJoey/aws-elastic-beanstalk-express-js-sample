6896f1574d22ab46f6f9df033c1081585f5d8c49
Joe <pandajoey77@gmail.com>
2021-05-29 14:52:49 +0100

    added more test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Aws Elastic Beanstalk gitops test'));
+app.get('/', (req, res) => res.send('Aws Elastic Beanstalk gitops test adding more test to test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
